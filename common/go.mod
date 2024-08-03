a5d9dfea0236990863bdf98cb08cb745e8228651
火龙果 <3389@qq.com>
2024-08-04 01:50:15 +0800

    创建proto协议和编译产生proto文件 命令需要在kit目录执行

--- common/go.mod
+++ common/go.mod
@@ -1,3 +1,5 @@
 module github.com/sunsnail0/go-tgf-game/common
 
 go 1.22.4
+
+require google.golang.org/protobuf v1.34.2
--- kit/proto/run.sh
+++ kit/proto/run.sh
@@ -1,1 +1,1 @@
-protoc --go_out=./../../common/pb --plugin=./mac/protoc-gen-go --proto_path=../../hall/internal/proto ../../hall/internal/proto/hall.proto
+protoc --go_out=./../common/pb --plugin=./proto/mac/protoc-gen-go --proto_path=../hall/internal/proto ../hall/internal/proto/hall.proto
